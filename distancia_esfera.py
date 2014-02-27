f96249a601a33b80cde44bbae412d7e148b35f1a
JuanDavid <juampis77@hotmail.com>
2014-02-27 12:09:33 -0500

    correccion punto 2

--- distancia_esfera.py
+++ distancia_esfera.py
@@ -12,10 +12,11 @@ lon1 = float(sys.argv[2])
 lat2 = float(sys.argv[3])
 lon2 = float(sys.argv[4])
 
-theta = (lat1-lat2)*math.pi/180.0
-phi = (lon1-lon2)*math.pi/180
-r = 6371
-
-d = r*math.acos(1 - (math.sin(theta)**2 + math.sin(phi)**2)/2)
-print d
+theta1 = math.pi/2 - lat1*math.pi/180.0
+theta2 = math.pi/2 - lat2*math.pi/180.0
+phi = (lon1-lon2)*math.pi/180.0
+r = 6378.137
+alpha = math.acos(math.cos(theta1)*math.cos(theta2)+math.sin(theta1)*math.sin(theta2)*math.cos(phi))
+x = alpha*r
+print x
 
